ed9db8af216c4849d7214fbe3c90d0fb93e4fa79
K.Y. <kirillisokay@icloud.com>
2024-09-05 17:57:47 +0300

    substrings

---
+++ odin/substring.rb
@@ -0,0 +1,14 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+  s = string.downcase.split
+  h = Hash.new(0)
+  dictionary.each do |word|
+    s.each do |w|
+      h[word] += 1 if w.include?(word)
+    end
+  end
+  p h
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
